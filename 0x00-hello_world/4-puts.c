25824580e319636c1bba1096e2f6de9ec944496b
olarbii <olarbii07@gmail.com>
2022-10-13 23:59:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,19 +3,15 @@
 
 
 /**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
- *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
-
-}
+	return (0);
